ca13b6826b59715cea57004339362215217fd9f7
Bart63 <bartek.durys@gmail.com>
2021-12-07 21:25:23 +0100

    Fix creating, add populating with data

--- Projekt/Create.sql
+++ Projekt/Create.sql
@@ -1,16 +1,22 @@
 ﻿USE master;
 GO
-IF EXISTS(SELECT 1 FROM master.dbo.sysdatabases WHERE NAME = 'restauracja') DROP DATABASE restauracja
+USE master;
 GO
-CREATE DATABASE restauracja
+IF EXISTS(SELECT 1 FROM master.dbo.sysdatabases WHERE NAME = 'restauracja') 
+BEGIN
+	ALTER DATABASE [restauracja] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE restauracja;
+	CREATE DATABASE restauracja
+	ALTER DATABASE [restauracja] SET MULTI_USER
+END
 GO
 USE restauracja
 GO
 
 CREATE TABLE potrawy(
 	id_potrawy smallint IDENTITY(1,1) PRIMARY KEY,
-	typ nvarchar NOT NULL,
-	nazwa nvarchar NOT NULL,
+	typ nvarchar(max) NOT NULL,
+	nazwa nvarchar(max) NOT NULL,
 	cena smallmoney NOT NULL,
 	gramatura smallint NOT NULL,
 	czy_dostepny bit NOT NULL DEFAULT 1,
@@ -22,7 +28,7 @@ GO
 CREATE TABLE stoliki(
 	id_stolika tinyint IDENTITY(1,1) PRIMARY KEY,
 	liczba_miejsc tinyint NOT NULL,
-	lokalizacja nvarchar NOT NULL,
+	lokalizacja nvarchar(max) NOT NULL,
 	czy_wolne bit NOT NULL DEFAULT 1,
 	CONSTRAINT CHK_liczba_os_stoliki CHECK (liczba_miejsc > 0)
 );
@@ -31,9 +37,9 @@ GO
 CREATE TABLE rezerwacje(
 	id_rezerwacji int IDENTITY(1,1) PRIMARY KEY,
 	id_stolika tinyint NOT NULL,
-	nazwisko nvarchar NOT NULL,
+	nazwisko nvarchar(max) NOT NULL,
 	data_rozpoczecia datetime NOT NULL DEFAULT GETDATE(),
-	data_zakonczenia datetime,
+	data_zakonczenia datetime NOT NULL,
 	liczba_osob smallint NOT NULL,
 	CONSTRAINT FK_rezerwacje_stoliki FOREIGN KEY(id_stolika) REFERENCES stoliki (id_stolika),
 	CONSTRAINT CHK_data_zakonczenia_rezerwacje CHECK (data_zakonczenia > data_rozpoczecia),
@@ -43,7 +49,7 @@ GO
 
 CREATE TABLE stanowiska(
 	id_stanowiska tinyint IDENTITY(1,1) PRIMARY KEY,
-	nazwa nvarchar NOT NULL,
+	nazwa nvarchar(max) NOT NULL,
 	placa_min smallmoney NOT NULL,
 	placa_max smallmoney NOT NULL,
 	zatrudnionych_min tinyint NOT NULL,
@@ -57,21 +63,20 @@ CREATE TABLE stanowiska(
 
  CREATE TABLE imprezy(
 	id_imprezy smallint IDENTITY(1,1) PRIMARY KEY,
-	nazwa nvarchar NOT NULL,
+	nazwa nvarchar(max) NOT NULL,
 	data_rozpoczecia datetime NOT NULL DEFAULT GETDATE(),
 	data_zakonczenia datetime,
 	liczba_osob smallint NOT NULL,
-	komentarz nvarchar NOT NULL,
+	komentarz nvarchar(max) NOT NULL,
 	CONSTRAINT CHK_data_zakonczenia_imprezy CHECK (data_zakonczenia > data_rozpoczecia),
 	CONSTRAINT CHK_liczba_os_imprezy CHECK (liczba_osob > 0)
  );
  GO
 
-
 CREATE TABLE pracownicy(
 	id_pracownika int IDENTITY(1,1) PRIMARY KEY,
-	imie nvarchar NOT NULL,
-	nazwisko nvarchar NOT NULL,
+	imie nvarchar(max) NOT NULL,
+	nazwisko nvarchar(max) NOT NULL,
 	plec char(1) NOT NULL,
 	id_stanowiska tinyint NOT NULL,
 	data_zatrudnienia datetime NOT NULL DEFAULT GETDATE(),
@@ -79,7 +84,7 @@ CREATE TABLE pracownicy(
 	placa smallmoney NOT NULL,
 	premia smallmoney,
 	CONSTRAINT FK_pracownicy_stanowiska FOREIGN KEY(id_stanowiska) REFERENCES stanowiska (id_stanowiska),
-	CONSTRAINT CHK_plec_pracownicy CHECK (plec IN ('M', 'F')),
+	CONSTRAINT CHK_plec_pracownicy CHECK (plec IN ('M', 'K')),
 	CONSTRAINT CHK_data_zwolnienia_pracownicy CHECK (data_zwolnienia > data_zatrudnienia),
 	CONSTRAINT CHK_placa_pracownicy CHECK (placa >= 0)
 );
@@ -90,11 +95,13 @@ GO
 	id_pracownika int NOT NULL,
 	data_rozpoczecia datetime NOT NULL DEFAULT GETDATE(),
 	data_zakonczenia datetime,
-	obecnosc nvarchar,
+	obecnosc nvarchar(max),
 	CONSTRAINT FK_zmiany_pracownicy FOREIGN KEY(id_pracownika) REFERENCES pracownicy (id_pracownika),
 	CONSTRAINT CHK_data_zakonczenia_zmiany CHECK (data_zakonczenia > data_rozpoczecia),
 	CONSTRAINT CHK_obecnosc_zmiany CHECK (obecnosc IN 
-		('nieobecność usprawiedliwiona', 'urlop na życzenie', 'urlop', 'nadgodziny', 'chorobowe', 'nieobecny', 'obecny')
+		('nieobecność usprawiedliwiona', 'nieobecność nieusprawiedliwiona', 
+		'urlop', 'nadgodziny', 'obecny'
+		)
 	)
 );
 GO
@@ -106,7 +113,7 @@ GO
 	liczba_porcji tinyint NOT NULL DEFAULT 1,
 	data_zlozenia datetime NOT NULL DEFAULT GETDATE(),
 	data_realizacji datetime,
-	komentarz nvarchar,
+	komentarz nvarchar(max),
 	id_kelnera int NOT NULL,
 	id_kucharza int,
 	CONSTRAINT FK_zamowienia_stoliki FOREIGN KEY(id_stolika) REFERENCES stoliki (id_stolika),
@@ -114,6 +121,6 @@ GO
 	CONSTRAINT FK_zamowienia_kelnera FOREIGN KEY(id_kelnera) REFERENCES pracownicy (id_pracownika),
 	CONSTRAINT FK_zamowienia_kucharza FOREIGN KEY(id_kucharza) REFERENCES pracownicy (id_pracownika),
 	CONSTRAINT CHK_liczba_porcji CHECK (liczba_porcji > 0),
-	CONSTRAINT CHK_data_realizacji_zamowienia CHECK (data_realizacji > data_zlozenia)
+	CONSTRAINT CHK_data_realizacji_zamowienia CHECK (data_realizacji >= data_zlozenia)
  );
  GO
---
+++ Projekt/Data.sql
@@ -0,0 +1,87 @@
+﻿USE restauracja;
+GO
+
+INSERT INTO potrawy VALUES 
+	('mięsny', 'stek', 63, 400, 1),
+	('mięsny', 'hamburger', 30, 270, 1),
+	('mięsny', 'schabowy panierowany', 20, 260, 1),
+	('mięsny', 'kotlet de voileile', 24, 290, 1),
+	('mięsny', 'drobiowy panierowany', 20, 260, 1),
+	('mięsny', 'golonka z kością', 6, 100, 0),
+	('wege', 'sałatka', 22, 200, 1),
+	('wege', 'naleśniki z owocami', 20, 250, 1),
+	('wege', 'pierogi z kapustą', 25, 300, 1),
+	('dodatki', 'surówka', 10, 100, 1),
+	('dodatki', 'frytki', 8, 100, 1),
+	('dodatki', 'kluski śląskie', 10, 150, 0),
+	('napoje', 'piwo', 10, 500, 1),
+	('napoje', 'cola', 8, 500, 1),
+	('napoje', 'sok jabłkowy', 7, 200, 1),
+	('napoje', 'herbata', 9, 200, 1),
+	('napoje', 'kawa', 8, 200, 0),
+	('napoje', 'woda', 6, 500, 1),
+	('deser', 'deser lodowy', 18, 200, 1),
+	('deser', 'szarlotka z bitą śmietaną', 18, 200, 0)
+	
+INSERT INTO stoliki VALUES 
+	(2, 'przód', 1),
+	(2, 'przód', 1),
+	(4, 'przód', 1),
+	(4, 'przód', 1),
+	(6, 'środek', 1),
+	(6, 'środek', 1),
+	(4, 'tył', 1),
+	(4, 'tył', 1),
+	(2, 'tył', 1),
+	(4, 'lewa ściana', 1),
+	(4, 'lewa ściana', 1),
+	(6, 'prawa ściana', 1)
+	
+DECLARE @cur_date datetime = GETDATE();
+INSERT INTO rezerwacje VALUES
+	(1, 'Nowak', DATEADD(HOUR, -6, @cur_date), DATEADD(HOUR, -5, @cur_date), 1),
+	(5, 'Kowalski', DATEADD(HOUR, -1, @cur_date), DATEADD(HOUR, 1, @cur_date), 5),
+	(1, 'Durys', @cur_date, DATEADD(HOUR, 1, @cur_date), 2),
+	(3, 'Klewicki', @cur_date, DATEADD(HOUR, 2, @cur_date), 3)
+
+INSERT INTO stanowiska VALUES
+	('kelner', 2500, 3000, 1, 3),
+	('kucharz', 2800, 3800, 2, 4)
+
+INSERT INTO imprezy VALUES
+	('urodziny', DATEADD(day, 1, @cur_date), DATEADD(day, 2, @cur_date), 23, 'jednodniowa impreza')
+
+INSERT INTO pracownicy VALUES
+	('Jan', 'Kowalski', 'M', 1, DATEADD(YEAR, -1, @cur_date), NULL, 2800, 100),
+	('Maria', 'Wójcik', 'K', 1, DATEADD(YEAR, -1, @cur_date), NULL, 3000, 0),
+	('Barbara', 'Wróbel', 'K', 1, DATEADD(YEAR, -1, @cur_date), DATEADD(MONTH, -2, @cur_date), 3000, 0),
+	('Henryk', 'Mazur', 'M', 2, DATEADD(YEAR, -1, @cur_date), NULL, 3300, 200),
+	('Alicja', 'Dąbrowska', 'K', 2, DATEADD(MONTH, -6, @cur_date), NULL, 3100, 0),
+	('Ryszard', 'Zając', 'M', 2, DATEADD(YEAR, -1, @cur_date), DATEADD(MONTH, -5, @cur_date), 3400, 0)
+
+INSERT INTO zmiany VALUES
+	(1, DATEADD(DAY, -1, @cur_date), DATEADD(HOUR, -14, @cur_date), 'obecny'),
+	(2, DATEADD(HOUR, -6, @cur_date), DATEADD(HOUR, 4, @cur_date), 'obecny'),
+	(4, DATEADD(DAY, -1, @cur_date), DATEADD(HOUR, -14, @cur_date), 'obecny'),
+	(5, DATEADD(HOUR, -6, @cur_date), DATEADD(HOUR, 4, @cur_date), 'obecny'),
+	(1, DATEADD(DAY, 1, @cur_date), DATEADD(HOUR, 34, @cur_date), 'obecny'),
+	(5, DATEADD(DAY, 1, @cur_date), DATEADD(HOUR, 34, @cur_date), 'obecny'),
+	(4, DATEADD(DAY, 1, @cur_date), DATEADD(DAY, 5, @cur_date), 'urlop')
+
+INSERT INTO zamowienia VALUES
+	(1, 13, 1, DATEADD(HOUR, -6, @cur_date), DATEADD(HOUR, -6, @cur_date), '', 2, NULL),
+	(5, 1, 2, DATEADD(HOUR, -1, @cur_date), @cur_date, 'medium rare', 2, 5),
+	(5, 2, 2, DATEADD(HOUR, -1, @cur_date), @cur_date, '', 2, 5),
+	(5, 9, 1, DATEADD(HOUR, -1, @cur_date), @cur_date, '', 2, 5),
+	(1, 15, 1, @cur_date, @cur_date, '', 2, NULL),
+	(3, 11, 2, @cur_date, @cur_date, '', 2, 5),
+	(3, 3, 2, @cur_date, @cur_date, '', 2, 5)
+
+SELECT * FROM potrawy
+SELECT * FROM stoliki
+SELECT * FROM rezerwacje
+SELECT * FROM stanowiska
+SELECT * FROM imprezy
+SELECT * FROM pracownicy
+SELECT * FROM zmiany
+SELECT * FROM zamowienia
